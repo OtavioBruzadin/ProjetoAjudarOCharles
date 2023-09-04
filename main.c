3e295a021ccab0726f680a0c2717dc522970b8f9
Otávio Bruzadin <31077442+OtavioBruzadin@users.noreply.github.com>
2023-09-04 09:00:02 -0300

    initial commit

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
+FILE  *ListaDeAlunos;
+
+
+void startFile(){
+    ListaDeAlunos = fopen("listaDeAlunos.txt","w");
+    fclose(ListaDeAlunos);
+}
+
+void writeOnFile(char *message){
+    ListaDeAlunos = fopen("listaDeAlunos.txt","a");
+    fprintf(ListaDeAlunos,"%s\n",message);
+    fclose(ListaDeAlunos);
+}
+
+
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+        startFile();
+        char palavra[] = "2,2,2,2,bosta,50";
+
+    char palavra2[] = "2,2,2,2,bosta,50";
+    writeOnFile(palavra);
+
+    writeOnFile(palavra2);
+
 }
